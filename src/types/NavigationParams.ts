f3b8d3bca29d0118e8ebbc10db65d8fe4e768c69
karseng9898 <65061122+karseng9898@users.noreply.github.com>
2021-12-01 18:33:33 +0800

    Revert "Authentication screen"

--- src/types/NavigationParams.ts
+++
@@ -1,9 +0,0 @@
-export type AppStackNavigationParam = {
-  AuthScreens: undefined;
-};
-
-export type AuthStackNavigationParam = {
-  AuthMenuScreen: undefined;
-  RegisterScreen: undefined;
-  LoginScreen: undefined;
-};
