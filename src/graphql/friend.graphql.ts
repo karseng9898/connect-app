e3512207b77a749ea2f10b97f3be4b9a11ac897c
Kar Seng <karseng9898@gmail.com>
2022-01-04 12:24:41 +0800

    added graphql queries

---
+++ src/graphql/friend.graphql.ts
@@ -0,0 +1,65 @@
+import { gql } from '@apollo/client';
+
+export interface Friend {
+  id: number;
+  senderId: number;
+  receiverId: number;
+  status: boolean;
+  createdAt: string;
+}
+
+const friendFields = `
+    id
+    senderId
+    receiverId
+    status
+    createdAt
+`;
+
+export const GET_FRIENDS = gql`
+    query friends {
+        friends {
+            ${friendFields}
+        }
+    }
+`;
+
+export const GET_FRIEND = gql`
+    query friend($id: Int!)  {
+        friend(id: $id) {
+            ${friendFields}
+        }
+    }
+`;
+
+export const CHECK_FRIEND_STATUS = gql`
+    query checkFriendStatus($id: Int!) {
+        checkFriendStatus(friendId: $id) {
+            ${friendFields}
+        }
+    }
+`;
+
+export const ADD_FRIEND = gql`
+    mutation addFriend($id: Int!) {
+        addFriend(receiverId: $id) {
+            ${friendFields}
+        }
+    }
+`;
+
+export const ACCEPT_FRIEND = gql`
+    mutation acceptFriend($id: Int!) {
+        acceptFriend(friendId: $id) {
+            ${friendFields}
+        }
+    }
+`;
+
+export const UNFRIEND = gql`
+  mutation unfriend($id: Int!) {
+    unfriend(friendId: $id) {
+      createdAt
+    }
+  }
+`;
